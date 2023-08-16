4604e42d13a1be554b4f6ca400d57600394291cb
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-15 21:35:38 -0300

    testando IDE

--- public/index.js
+++ public/index.js
@@ -1,2 +1,13 @@
 
+/**
+ * Função de teste para ver se a IDE está funcionando corretamente
+ * @param {number} value 
+ * @returns {void}
+ */
+function funcaoComTipo(value) {
+    console.log(value);
+}
+
+
 console.log("Olá mundo dos boids.");
+funcaoComTipo(5);
