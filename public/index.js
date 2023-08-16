316e92b624f00df6ccf9e2f3c790ca6fe7e3395e
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-15 21:52:19 -0300

    loop setado

--- public/index.js
+++ public/index.js
@@ -1,13 +1,58 @@
 
-/**
- * Função de teste para ver se a IDE está funcionando corretamente
- * @param {number} value 
- * @returns {void}
- */
-function funcaoComTipo(value) {
-    console.log(value);
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 400;
+
+const canvas = document.createElement('canvas');
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+document.body.append(canvas);
+
+const ctx = canvas.getContext('2d');
+
+ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+
+class AnimationFrameLoop {
+  running = false;
+  handle = null;
+  lastRequestId = 0;
+
+
+  /**
+   * @private
+   * @param {number} timestamp 
+   */
+  handleAnimationFrame = (timestamp) => {
+
+    const handle = this.handle;
+    // @todo calcular deltatime quando necessário
+    const deltaTime = 0;
+
+    try {
+      if (typeof handle == 'function') handle(timestamp, deltaTime);
+    } catch(error) {
+      console.error(error)
+    }
+
+    if (this.running) {
+      this.lastRequestId = requestAnimationFrame(this.handleAnimationFrame);
+    }
+  };
+
+  start = () => {
+    if (!this.running) {
+      this.running = true;
+      this.lastRequestId = requestAnimationFrame(this.handleAnimationFrame);
+    }
+  }
 }
 
+function handleFrame(timestamp, deltaTime) {
+  console.log(`timestamp: ${timestamp}, deltaTime: ${deltaTime}`);
+}
 
-console.log("Olá mundo dos boids.");
-funcaoComTipo(5);
+const loop = new AnimationFrameLoop();
+loop.handle = handleFrame;
+loop.start();
