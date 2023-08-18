6277d12b575b7fb5314c1a16f61c9754721ca963
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-17 22:22:06 -0300

    SUB

--- public/vector2-math.js
+++ public/vector2-math.js
@@ -22,3 +22,23 @@ export const addInPlace = (a, b) => {
   a.y += b.y;
   return a;
 };
+
+/**
+ * @reference https://natureofcode.com/book/chapter-1-vectors/#vector-subtraction
+ * 
+ * @param {Vector2} a 
+ * @param {Vector2} b 
+ * @returns {Vector2}
+ */
+export const sub = (a, b) => ({ x: a.x - b.x, y: a.y - b.y, });
+
+/**
+ * @param {Vector2} a vetor que terá o seu valor incrementado
+ * @param {Vector2} b valor a ser adicionado ao outro vetor
+ * @returns {Vector2}
+ */
+export const subInPlace = (a, b) => {
+  a.x -= b.x;
+  a.y -= b.y;
+  return a;
+};
