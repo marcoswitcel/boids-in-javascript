55166dd4ca8acff73ba033689c1fca157b594f25
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-17 23:05:55 -0300

    division

--- public/vector2-math.js
+++ public/vector2-math.js
@@ -84,6 +84,46 @@ export const scalarMulInPlace = (a, n) => {
 };
 
 /**
+ * @reference https://natureofcode.com/book/chapter-1-vectors/#vector-multiplication
+ * 
+ * @param {Vector2} a  
+ * @param {Vector2} b  
+ * @returns {Vector2}
+ */
+export const div = (a, b) => ({ x: a.x / b.x, y: a.y / b.y, });
+
+/**
+ * @param {Vector2} a
+ * @param {Vector2} b
+ * @returns {Vector2}
+ */
+export const divInPlace = (a, b) => {
+  a.x /= b.x;
+  a.y /= b.y;
+  return a;
+};
+
+/**
+ * @reference https://natureofcode.com/book/chapter-1-vectors/#vector-multiplication
+ * 
+ * @param {Vector2} a  
+ * @param {number} n  
+ * @returns {Vector2}
+ */
+export const scalarDiv = (a, n) => ({ x: a.x / n, y: a.y / n, });
+
+/**
+ * @param {Vector2} a
+ * @param {number} n
+ * @returns {Vector2}
+ */
+export const scalarDivInPlace = (a, n) => {
+  a.x /= n;
+  a.y /= n;
+  return a;
+};
+
+/**
  * @reference https://natureofcode.com/book/chapter-1-vectors/#15-vector-magnitude
  * 
  * @param {Vector2} v vetor sendo analisado
