a42df4e6ed28418ffe2cf2071c1acf03a7de80af
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-17 22:53:30 -0300

    multiplication

--- public/vector2-math.js
+++ public/vector2-math.js
@@ -33,12 +33,32 @@ export const addInPlace = (a, b) => {
 export const sub = (a, b) => ({ x: a.x - b.x, y: a.y - b.y, });
 
 /**
- * @param {Vector2} a vetor que terá o seu valor incrementado
- * @param {Vector2} b valor a ser adicionado ao outro vetor
+ * @param {Vector2} a vetor que terá o seu subtraído
+ * @param {Vector2} b valor a ser subtraído do outro vetor
  * @returns {Vector2}
  */
 export const subInPlace = (a, b) => {
   a.x -= b.x;
   a.y -= b.y;
+  return a;
+};
+
+/**
+ * @reference https://natureofcode.com/book/chapter-1-vectors/#vector-multiplication
+ * 
+ * @param {Vector2} a  
+ * @param {Vector2} b  
+ * @returns {Vector2}
+ */
+export const mul = (a, b) => ({ x: a.x * b.x, y: a.y * b.y, });
+
+/**
+ * @param {Vector2} a
+ * @param {Vector2} b
+ * @returns {Vector2}
+ */
+export const mulInPlace = (a, b) => {
+  a.x *= b.x;
+  a.y *= b.y;
   return a;
 };
