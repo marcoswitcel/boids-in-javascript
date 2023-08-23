ec26791ae39b66e055d3179b2a788b67416a521a
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-22 21:50:35 -0300

    formatação

--- public/utils.js
+++ public/utils.js
@@ -1,6 +1,6 @@
 
 export const setDocumentTitle = (newTitle) => {
-    document.title = newTitle;
+  document.title = newTitle;
 };
 
 export const between = (value, min, max) => Math.max(Math.min(value, max), min);
